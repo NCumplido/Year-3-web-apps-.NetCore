bc05b8298275ba26764583d865c8bce73d9d7687
Nic <ncumplido@msn.com>
2019-01-01 15:11:09 +0000

    Create Comment.cs

---
+++ 826488CW2/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _826488CW2.Models
+{
+    public class Comment
+{
+        [Key, ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2), MaxLength(60)]
+        public string Content { get; set; }
+    }
+}
