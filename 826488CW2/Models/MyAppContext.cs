eb5b0cc65ee4e113412fc686947bb2ef40a1b17b
Nic <ncumplido@msn.com>
2019-01-03 14:39:43 +0000

    Revert "Revert "Add MyAppContext""
    This reverts commit 9735639acec656085b74169cdd3db74ae58a3856.

---
+++ 826488CW2/Models/MyAppContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _826488CW2.Models
+{
+    public class MyAppContext : IdentityDbContext<ApplicationUser>
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options)
+            :base (options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
