fadb1d4821cf1dd93085db92c331ba30cdda3a98
Nic <ncumplido@msn.com>
2019-01-06 11:38:59 +0000

    Add Posts attribute to ApplicationUser

--- 826488CW2/Models/ApplicationUser.cs
+++ 826488CW2/Models/ApplicationUser.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace _826488CW2.Models
 {
+    //Client class
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
@@ -14,5 +15,6 @@ namespace _826488CW2.Models
         public string Name { get; set; }
         //ToDo: Add data annotations
         public  DateTime Birthdate { get; set; }
+        public ICollection<Post> Posts { get; set; }
     }
 }
